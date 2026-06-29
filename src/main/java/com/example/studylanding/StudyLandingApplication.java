50ded81de8e0aca1c01d55c0c50ba73eb1cf315c
Alexander1985TV <svz.tav@yandex.com>
2026-06-29 15:00:40 +0300

    Initial commit

---
+++ src/main/java/com/example/studylanding/StudyLandingApplication.java
@@ -0,0 +1,13 @@
+package com.example.studylanding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyLandingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyLandingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/studylanding/StudyLandingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studylanding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyLandingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
